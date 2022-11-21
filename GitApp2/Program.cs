b694d48889293475eb4c10219e7677d284dfc5cd
Evgeny <es.zaharov@yandex.ru>
2022-11-21 12:40:32 +0300

    master commit 1

--- GitApp2/Program.cs
+++ GitApp2/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("I create newBranch_!");
+            Console.WriteLine("I Add commit master 1");
         }
     }
 }
