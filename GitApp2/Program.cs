5064260818b00968bf6e7c72dd2478aa72445f4f
Evgeny <es.zaharov@yandex.ru>
2022-11-21 12:56:39 +0300

    DELETE ALL

--- GitApp2/Program.cs
+++ GitApp2/Program.cs
@@ -4,30 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("I create newBranch_!");
-
-            Console.WriteLine("I Add commit newBranch_1 1");
-
-            Console.WriteLine("I Add commit master 1");
-            Console.WriteLine("I Add commit master 2");
-
-            Console.WriteLine("GLOBAL UPDATE");
-            Console.WriteLine("GLOBAL UPDATE");
-            Console.WriteLine("GLOBAL UPDATE");
-            Console.WriteLine("GLOBAL UPDATE");
-            Console.WriteLine("GLOBAL UPDATE");
-            Console.WriteLine("GLOBAL UPDATE");
-            Console.WriteLine("GLOBAL UPDATE");
-            Console.WriteLine("GLOBAL UPDATE");
-            Console.WriteLine("GLOBAL UPDATE");
-            Console.WriteLine("GLOBAL UPDATE");
-
-            Console.WriteLine("GLOBAL UPDATE TOO");
-            Console.WriteLine("GLOBAL UPDATE TOO");
-            Console.WriteLine("GLOBAL UPDATE TOO");
-
-
+            Console.WriteLine("DELETE ALL!");
         }
     }
 }
